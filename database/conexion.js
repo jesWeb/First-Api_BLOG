4f2045a8264ac39b50ec32f4224faefdce64ba3a
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2024-10-28 21:51:30 -0600

    first commit

---
+++ database/conexion.js
@@ -0,0 +1,21 @@
+//conexion a base de datos 
+const mongoose = require("mongoose")
+
+const conexion = async() => {
+
+    try {
+        await mongoose.connect("mongodb://localhost:27017/MI_blog");
+        
+        console.log('Conectado correctamete a mongo ')
+
+    } catch (error) {
+        console.log(error)
+        throw new Error("No se ha pdido establecer la conexion ala base de datos !!");
+             
+    }
+
+}
+
+module.exports = {
+    conexion
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const {conexion} = require("./database/conexion")
+
+//inicializar la app 
+console.log("App conectada correctamente");
+
+//conectar a la base de datos 
+conexion();
