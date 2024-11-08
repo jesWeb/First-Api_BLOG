ebdaa234a7c5af9a41db1319cee55ecd046f046a
jesus Gutierrez <jesuslgutierrez685@gmail.com>
2024-11-07 23:52:34 -0600

    Creamos los metodos http get put delete y respectivas riutas

--- controladores/articulo.js
+++ controladores/articulo.js
@@ -75,17 +75,163 @@ const crear = async (req, res) => {
                 mensaje: "no se ha guardado el post "
             });
         }
-
-
     }
-
-
 
     /**nota ya no acepta los callbakc osea el codigo de abajo anterior   */
 }
 
+//get articulos 
+/***
+  *  nota busca para que se usa el .exec 
+  * esta funcion ya no es aceptada y se deve de manejar como promesas  que es para las consultas
+  * 
+  * Antes: Usabas exec() con un callback.
+     Ahora: exec() devuelve una promesa, por lo que debes manejarlo con async/await o .then().   
+     Asegúrate de revisar todo tu código y hacer estos cambios en todas las consultas que utilicen exec() con un callback.
+  * 
+  * 
+  * 
+  *   */
+
+const listar = async function lista(req, res) {
+    try {
+        let consulta = Articulo.find({});
+        //filtros de parametros 
+        if (req.params.ultimos) {
+            consulta.limit(3);
+        }
+
+        const articulos = await consulta.sort({
+            fecha: -1
+        });
+
+        return res.status(200).json({
+            mensaje: "success",
+            contador: articulos.length,
+            articulos
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            status: "error",
+            mensaje: "no se ha encontrado el post"
+        });
+    }
+}
+
+
+//sacar uno elemento 
+
+const uno = async (req, res) => {
+
+    try {
+
+        let id = req.params.id;
+
+        const obtenerUno = await Articulo.findById(id);
+
+        return res.status(200).json({
+            mensaje: "success has obtenido el articulo con el id ",
+            obtenerUno
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            status: "error",
+            mensaje: "no se ha encontrado el post"
+        });
+    }
+
+
+}
+
+//metodo de borrar
+
+const borrar = async (req, res) => {
+
+    try {
+
+        let articulo_id = req.params.id;
+
+        const borrarArticulos = await Articulo.findOneAndDelete({
+            _id: articulo_id
+        });
+
+        return res.status(200).json({
+            status: 'success',
+            articulo: borrarArticulos,
+            mensaje: "success has eliminado el articulo con el exito ",
+        });
+
+
+
+
+    } catch (error) {
+        return res.status(400).json({
+            status: "error",
+            mensaje: "no se ha eliminado el post"
+        });
+    }
+}
+
+//editar 
+
+const editar = async (req,res) => {
+
+   
+    try {
+        //recoger elarticulo ID  a editar 
+        let articuloID = req.params.id;
+
+        //rerocger parametros del body 
+        let parametros = req.body;
+
+        let validar_titulo = !validator.isEmpty(parametros.titulo) &&
+            validator.isLength(parametros.titulo, {
+                min: 5,
+                max: undefined
+            });
+
+        let validar_content = !validator.isEmpty(parametros.contenido);
+
+        if (!validar_titulo || !validar_content) {
+            throw new Error("NO se ha validado la informacion !!")
+        };
+
+        //buscar y actualizar el articlo 
+
+        const ActualizarArt = await Articulo.findOneAndUpdate({
+            _id: articuloID
+        }, req.body, {
+            new: true
+        });
+
+        return res.status(200).json({
+            status: 'success',
+            articulo: ActualizarArt,
+            mensaje: "success has editado articulo con el exito ",
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            status: "error",
+            mensaje: "No se ha podido actualizar "
+        });
+    }
+
+
+
+
+}
+
+
+
 module.exports = {
     prueba,
     curso,
-    crear
+    crear,
+    listar,
+    uno,
+    borrar,
+    editar
 }
--- rutas/articulo.js
+++ rutas/articulo.js
@@ -1,15 +1,19 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 //definir controllador 
 const ArticuloController = require("../controladores/articulo") ;
 const { crear } = require("../controladores/articulo");
-//rutas de preuba 
 
+//rutas de preuba 
 router.get("/ruta-de-pruebas", ArticuloController.prueba);
 router.get("/curso", ArticuloController.curso);
 
 //ruta util 
 router.post("/crear",ArticuloController.crear);
+router.get("/articulos/:ultimos?",ArticuloController.listar);
+router.get('/articulos/:id',ArticuloController.uno);
+router.delete('/articulos/:id',ArticuloController.borrar);
+router.put('/articulos/:id',ArticuloController.editar);
 
 module.exports = router;
